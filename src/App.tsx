5301d486c1f74b4055eebc78850c5adacdc11700
Rodrigo Roldán <rodrigo_roldan18@hotmail.com>
2024-06-25 20:32:52 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { FooterComponent } from "./components/FooterComponent"
+import { HeaderComponent } from "./components/HeaderComponent"
+
+export const App = () => {
+  return (
+    <>
+        <HeaderComponent />
+        <FooterComponent />
+    </>
+  )
+}
---
+++ src/components/HeaderComponent.tsx
@@ -0,0 +1,7 @@
+
+export const HeaderComponent = () => {
+  return (
+    <div>HeaderComponent</div>
+  )
+}
+
